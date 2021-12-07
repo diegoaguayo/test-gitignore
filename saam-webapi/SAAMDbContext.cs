3e7eefba309dd247a5f4dc391bbc47bd85932815
Rodrigo Suárez <e_rsuarez@saamsa.com>
2021-12-07 19:10:04 -0300

    Estructura de carpetas y LocalDB

---
+++ saam-webapi/SAAMDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using saam_webapi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace saam_webapi
+{
+    public class SAAMDbContext : DbContext
+    {
+        public SAAMDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Especialidad> Especialidades { get; set; }
+    }
+}
--- saam-webapi/Startup.cs
+++ saam-webapi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,7 +27,8 @@ namespace saam_webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<SAAMDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("stringConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
